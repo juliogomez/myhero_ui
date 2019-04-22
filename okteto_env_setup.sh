3a9669866e6c56e7d163279ac88070bc0ea7fd86
developer <developer@devnetsandbox.cisco.com>
2019-04-22 20:12:59 +0200

    remote okteto env setup script

--- okteto_env_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-echo "Configuring UI to use APP Server: $myhero_app_server"
-sed -i -e 's#MYHERO_SPARK_SERVER#'"$myhero_spark_server"'#g' $(pwd)/app/scripts/app.js
-sed -i -e 's#MYHERO_APP_SERVER#'"$myhero_app_server"'#g' $(pwd)/app/scripts/app.js
-sed -i -e 's/MYHERO_APP_KEY/'"$myhero_app_key"'/g' $(pwd)/app/scripts/app.js
